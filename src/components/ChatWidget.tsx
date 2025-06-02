17f849d53bca874ba8d783dee97a526e6daf0b52
ROOBAN <roopavanan1009@gmail.com>
2025-06-02 10:37:06 +0530

    voca update

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
+import { IoMdSend } from "react-icons/io";
+
 
 export default function ChatWidget() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,9 +14,9 @@ export default function ChatWidget() {
       {/* Floating Chat Icon */}
      <button
   onClick={() => setIsOpen(!isOpen)}
-  className="fixed bottom-5 right-6 z-50 w-16 h-16 bg-white rounded-full shadow-lg flex items-center justify-center"
+  className="fixed bottom-5 right-6 z-50 w-16 h-16  rounded-full shadow-lg flex items-center justify-center"
 >
-  <Image src="/images/bot.svg" alt="Chatbot Icon" width={40} height={40} />
+  <Image src="/images/bot.svg" alt="Chatbot Icon" width={60} height={60} />
 </button>
 
 
@@ -26,7 +28,7 @@ export default function ChatWidget() {
             animate={{ opacity: 1, y: 0, scale: 1 }}
             exit={{ opacity: 0, y: 10, scale: 0.95 }}
             transition={{ duration: 0.3 }}
-            className="fixed bottom-24 right-6 z-50 w-72 md:w-80 bg-[#FEEFEB] rounded-3xl shadow-xl p-4 text-center"
+            className="fixed bottom-24 right-15 z-50 w-72 md:w-80 bg-[#FEF0D8]  border-2 border-[#FCD799] rounded-3xl shadow-xl p-6 text-center"
           >
               {/* Close Button */}
             <button
@@ -37,27 +39,28 @@ export default function ChatWidget() {
               ×
             </button>
             
-            <p className="text-sm font-medium text-[#EE842C] font-primary">Hi</p>
-            <h3 className="text-lg font-semibold text-[#AD4027] italic mt-1 font-primary">I&apos;m VOCA</h3>
+            <p className="text-sm font-medium text-[#B28842] font-primary">Hi</p>
+            <h3 className="text-lg font-semibold text-[#B28842] italic mt-1 font-primary">I&apos;m VOCA</h3>
 
             <Image
               src="/images/bot.svg"
               alt="Chatbot"
-              width={60}
-              height={60}
+              width={80}
+              height={80}
               className="mx-auto mb-4"
             />
-                        <p className="text-sm text-[#AD4027] font-primary mb-4">How can I help you?</p>
+                        <p className="text-sm text-[#B28842] font-primary mb-4">How can I help you?</p>
 
 
             <div className="relative">
               <input
                 type="text"
                 placeholder="Type your message here..."
-                className="w-full rounded-full border px-4 py-2 text-sm text-gray-700 outline-none focus:ring-2 focus:ring-[#F45A37]"
+                className="w-full rounded-full border px-4 py-2 text-sm text-gray-700 bg-white outline-none focus:ring-2 focus:ring-[#FCD799]"
               />
-              <button className="absolute right-1 top-1 bottom-1 px-3 bg-[#F45A37] text-white rounded-full text-sm">
-                →
+              <button className="absolute right-1 top-1 bottom-1 px-3  text-grey-200 rounded-full text-sm">
+               <IoMdSend />
+
               </button>
             </div>
           </motion.div>
