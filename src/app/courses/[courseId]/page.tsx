2783a578ecce40473a5537078b6f30fa47581c08
ROOBAN <roopavanan1009@gmail.com>
2025-06-05 17:13:49 +0530

    error

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -1,20 +1,16 @@
+'use client';
+
 import { categories } from "@/data/courseData";
 import { notFound } from "next/navigation";
+import { useParams } from "next/navigation";
 import CourseList from "@/components/CourseList";
-interface Props {
-  params: {
-    courseId: string;
-  };
-}
 
-export default function CoursePage({ params }: Props) {
-  const courseId = params.courseId;
+export default function CoursePage() {
+  const { courseId } = useParams() as { courseId: string };
   const baseClass = courseId.split("-").slice(0, -1).join("-");
   const filtered = categories.filter((cat) => cat.id.startsWith(baseClass));
 
   if (filtered.length === 0) return notFound();
 
-  return (
-    <CourseList baseClass={baseClass} courses={filtered} />
-  );
+  return <CourseList baseClass={baseClass} courses={filtered} />;
 }
